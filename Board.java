9c37049ee056da7bd41b19f700a7c68e5ae492e3
Michael Paul Carson <michaelpaulcarson@Michaels-MacBook-Air-6.local>
2024-10-06 19:54:05 -0400

    Michael's Stubs

---
+++ Board.java
@@ -0,0 +1,20 @@
+/**
+ * This class sets up the board for the board game the user plays 
+ * @author Michael Carson 
+ */
+public class Board {
+    private int rows;
+    private int columns;
+    private int grid;
+
+    
+    public Board(){
+        return ;
+    }
+    public boolean setup(){
+        return true;
+    }
+    public int getSize(){
+        return 0;
+    }
+}
