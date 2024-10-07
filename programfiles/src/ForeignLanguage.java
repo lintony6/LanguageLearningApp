54ee4de3ab042f64be46be6348a43e5a2603d5c2
Michael Paul Carson <michaelpaulcarson@Michaels-MacBook-Air-6.local>
2024-10-06 20:33:13 -0400

    added initial commit #13 stubs

---
+++ programfiles/src/ForeignLanguage.java
@@ -0,0 +1,11 @@
+/**
+ * this enumeration holds the different language types the app offers 
+ * @author Michael Carson 
+ */
+
+public enum ForeignLanguage {
+SPANISH,
+GERMAN,
+FRENCH,
+CHINESE
+}
