5715c4154142f28dcb4ea3bfac7b4c1a43ff3288
Tony <tl27@email.sc.edu>
2024-10-06 21:39:34 -0400

    made updates to driver, #19, #20 for testing

--- programfiles/src/Driver.java
+++ programfiles/src/Driver.java
@@ -2,11 +2,15 @@
 public class Driver {
   public static void main(String[] args) {
     DataLoader loader = new DataLoader();
+    DataWriter writer = new DataWriter();
     try {
       loader.openFile();
       String data = (String) loader.loadData();
       loader.closeFile();
       System.out.println(data);
+      writer.openFile();
+      writer.writeData("This is testing the data writer");
+      writer.closeFile();
     } catch (Exception e) {
       System.out.println("File not found");
     }
