5452ab19f5209c2515911d84708ee876e98402a2
Tony <tl27@email.sc.edu>
2024-10-10 13:39:17 -0400

    updated #20

--- programfiles/src/dataloader.java
+++ programfiles/src/dataloader.java
@@ -1,12 +1,21 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
+
 public class DataLoader {
     private String filePath;
-    private String fileFormat;
     private BufferedReader reader;
+    private List<User> userList;
 
-    public Object loadData() throws IOException {
+    public DataLoader() {
+        userList = new ArrayList<>();
+    }
+
+    public List<User> loadData() throws IOException {
         if (reader == null) {
             throw new IOException("File is not open.");
         }
@@ -18,8 +27,8 @@ public class DataLoader {
         return parseData(content.toString());
     }
 
-    public void openFile() throws IOException {
-        this.filePath = "programfiles/src/data.txt";
+    public void openFile(String filePath) throws IOException {
+        this.filePath = filePath;
         reader = new BufferedReader(new FileReader(filePath));
     }
 
@@ -29,16 +38,38 @@ public class DataLoader {
         }
     }
 
-    public Object parseData(String content) {
-        // Implement your data parsing logic here
-        return content;
+    public List<User> parseData(String content) {
+        Gson gson = new Gson();
+        try {
+            User[] users = gson.fromJson(content, User[].class);
+            for (User user : users) {
+                userList.add(user);
+            }
+        } catch (JsonSyntaxException e) {
+            e.printStackTrace();
+        }
+        return userList;
     }
 
     public void setFilePath(String filePath) {
         this.filePath = filePath;
     }
 
-    public void setFileFormat(String fileFormat) {
-        this.fileFormat = fileFormat;
+    public List<User> getUserList() {
+        return userList;
+    }
+
+    public static void main(String[] args) {
+        DataLoader dataLoader = new DataLoader();
+        try {
+            dataLoader.openFile(DataConstants.DEFAULT_FILE_PATH);
+            List<User> users = dataLoader.loadData();
+            dataLoader.closeFile();
+            for (User user : users) {
+                System.out.println(user);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
