d86f67bd70ca092fd30290d503dee1e4b775ac52
Tony <tl27@email.sc.edu>
2024-10-12 11:16:53 -0400

    updated #21

--- programfiles/src/UserList.java
+++ programfiles/src/UserList.java
@@ -1,7 +1,6 @@
 import java.util.HashMap;
 import java.util.UUID;
 import java.util.Map;
-import java.util.UUID;;
 /**
  * The class UserList maintains a collection of Users using a 
  * HashMap with UUID as the key and the User as the value;
