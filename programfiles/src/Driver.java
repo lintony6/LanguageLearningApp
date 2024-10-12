ccb80ee6370a630afe22b3dd34c34ec6473208bd
Tony <tl27@email.sc.edu>
2024-10-12 09:59:29 -0400

    updated driver to test valid login scenario

--- programfiles/src/Driver.java
+++ programfiles/src/Driver.java
@@ -2,13 +2,17 @@ import java.util.ArrayList;
 public class Driver {
   public static void main(String[] args) {
       try {
-        String filePath = "";   //enter your file path here
-        ArrayList<User> userList = DataLoader.loadUsers(filePath);
-        for(User user : userList) {
-          System.out.println(user.getFirstName() + " " +user.getUserID());
+        String filePath = "CSCE247/LanguageLearningApp/json/user.json";   //enter your file path here
+        UserList userList = DataLoader.loadUsers(filePath);
+        User user = userList.login("john_doe123","extremelysecure");
+        ArrayList<User> userFriends = user.getFriendList();
+        System.out.println(user.getFirstName() + " " +user.getUserID()
+          + "\n" +user.getSettings() + "\nFriends");
+        for(int i = 0; i < userFriends.size(); ++i) {
+          System.out.println(userFriends.get(i).getUserName());
         }
     } catch (Exception e) {
-        System.out.println("File not found");
+        System.out.println(e);
   }
  }
 }
