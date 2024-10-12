38432164cbe2b3d6074cf5d67c0b789a3df110f1
Tony <tl27@email.sc.edu>
2024-10-11 21:30:18 -0400

    added #18

---
+++ programfiles/src/DataConstants.java
@@ -0,0 +1,12 @@
+
+public class DataConstants {
+  protected static final String FILE_PATH = "../../json/user.json";
+  protected static final String USER_ID = "uuid";
+  protected static final String USER_FIRST_NAME = "first-name";
+  protected static final String USER_LAST_NAME = "last-name";
+  protected static final String USER_USERNAME = "username";
+  protected static final String USER_PASSWORD = "password";
+  protected static final String USER_PHONE_NUMBER = "phone-number";
+  protected static final String USER_LANGUAGE = "language";
+  protected static final String USER_LESSONPROGRESS = "15";
+}
