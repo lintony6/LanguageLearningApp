99212a1ed173ac17c1e1829cc5f4fc1ca8173294
Tony <tl27@email.sc.edu>
2024-10-12 11:13:28 -0400

    updated Driver to have mock login/signup interactions

--- programfiles/src/Driver.java
+++ programfiles/src/Driver.java
@@ -1,18 +1,77 @@
 import java.util.ArrayList;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.UUID;
 public class Driver {
-  public static void main(String[] args) {
-      try {
-        String filePath = "CSCE247/LanguageLearningApp/json/user.json";   //enter your file path here
-        UserList userList = DataLoader.loadUsers(filePath);
-        User user = userList.login("john_doe123","extremelysecure");
-        ArrayList<User> userFriends = user.getFriendList();
-        System.out.println(user.getFirstName() + " " +user.getUserID()
-          + "\n" +user.getSettings() + "\nFriends");
-        for(int i = 0; i < userFriends.size(); ++i) {
-          System.out.println(userFriends.get(i).getUserName());
-        }
-    } catch (Exception e) {
-        System.out.println(e);
+
+  private static LanguageLearningSystemFacade facade;
+  private static String filePath = "CSCE247/LanguageLearningApp/json/user.json";   //enter your file path here
+  private static UserList userList;
+  private static Scanner keyboard;
+  public static void startDemo() {
+    if(facade == null)
+      facade = LanguageLearningSystemFacade.getInstance();
+    userList = UserList.getInstance();
+    keyboard = new Scanner(System.in);
+    System.out.println("Enter 1 to login\nEnter 2 to signup");
   }
+
+  public static void login() {
+    try {
+    System.out.println("Enter your username");
+    String userName = keyboard.next();
+    keyboard.nextLine();
+    System.out.println("Enter your password");
+    String password = keyboard.next();
+    keyboard.nextLine();
+    facade.login(userName, password);
+    System.out.println("Welcome " + facade.getUser().getFirstName());
+    } catch(Exception e) {
+      System.out.println("Incorrect username/password");
+    }
+  }
+
+  public static void signUp() {
+    try {
+      System.out.println("Enter your first name");
+      String firstName = keyboard.nextLine();
+      System.out.println("Enter your last name");
+      String lastName = keyboard.nextLine();
+      System.out.println("Enter your username");
+      String userName = keyboard.nextLine();
+      System.out.println("Enter your password (At least 8 characters)");
+      String password = keyboard.nextLine();
+      if(password.length() <8) {
+        System.out.println("Please enter a valid password");
+        signUp();
+        return;
+      }
+      UUID newUserID = UUID.randomUUID();
+      facade.signUp(firstName, lastName, userName, password, newUserID);
+      System.out.println("Welcome " + facade.getUser().getFirstName());
+    } catch (Exception e) {
+      System.out.println(e);
+    }
+  }
+
+  public static void action() {
+    try {
+      int action = keyboard.nextInt();
+      keyboard.nextLine();
+      switch(action) {
+      case 1: login(); break;
+      case 2: signUp(); break;
+      case 9: System.exit(0);
+      default: System.out.println("Please enter 1 or 2");
+      }
+    } catch (Exception e) {
+      System.out.println("Please enter 1 or 2");
+    }
+    keyboard.nextLine();
+  }
+
+  public static void main(String[] args) {
+    startDemo();
+    action();
  }
 }
