1b087ba92eb909849df2f556c8bfabb964ef9454
aqw tiqz niqz <bl37@email.sc.edu>
2024-10-14 09:21:54 -0400

    merging

--- programfiles/src/picturestory.java
+++ programfiles/src/picturestory.java
@@ -17,4 +17,25 @@ public class PictureStory {
     }
 
     // Additional methods to add words and images can be added here
+    
+    // Method to add a single word to a specified page
+    public void addWordToPage(int pageNumber, Word word) {
+        pages.putIfAbsent(pageNumber, new ArrayList<>()); // Create the page if it doesn't exist
+        pages.get(pageNumber).add(word); // Add the word to the specified page
+    }
+
+    // Method to get the words for the current page
+    public ArrayList<Word> getCurrentPageWords() {
+        return pages.getOrDefault(currentPage, new ArrayList<>()); // Return words or an empty list if no words are present
+    }
+
+    // Method to set the current page
+    public void setCurrentPage(int pageNumber) {
+        if (pages.containsKey(pageNumber)) {
+            currentPage = pageNumber; // Set to the specified page if it exists
+        } else {
+            System.out.println("Page " + pageNumber + " does not exist.");
+        }
+    }
 }
+
