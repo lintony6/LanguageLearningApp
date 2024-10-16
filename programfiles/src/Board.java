3615773bf4b0fb7a1de3a9e0cd1b2500adf11676
Michael Paul Carson <michaelpaulcarson@Michaels-MacBook-Air-6.local>
2024-10-16 13:57:36 -0400

    cleaning up files

--- programfiles/src/Board.java
+++ programfiles/src/Board.java
@@ -1,3 +1,4 @@
+package LanguageLearningApp.programfiles.src;
 /**
  * This class sets up the board for the board game the user plays 
  * @author Michael Carson 
@@ -11,12 +12,14 @@ public class Board {
     public Board(){
         this(15, 15);
     }
+
     public Board(int rows, int columns){
         this.rows = rows;
         this.columns = columns;
         this.grid = new String[rows][columns];
         initilizeBoard();
     }
+    //initializes the board with the default values 
     private void initilizeBoard(){
         for (int i = 0; i < rows; i++){
             for (int j = 0; j < columns; j++){
@@ -24,13 +27,14 @@ public class Board {
             }
         }
     }
+    //prints the board
     public void printBoard(){
         for (int i = 0; i < rows; i++){
             for (int j = 0; j < columns; j++){
-        System.out.println(grid[i][j] +"");
+        System.out.println(grid[i][j] + " ");
+        }
     }
 }
-    }
     public boolean setup(){
         return true;
     }
---
+++ programfiles/src/BoardGame.java
@@ -0,0 +1,43 @@
+
+/**
+ * this class has all the functionality and implementation of the board game 
+ * @author Michael Carson 
+ */
+public class BoardGame {
+    private static BoardGame instance;
+    private Board boardgame;
+    private Player player;
+    private User user;
+    
+    private BoardGame(){
+        this.boardgame = new Board();
+        this.player = new Player(user);
+    }
+    public static BoardGame getInstance(){
+        if (instance == null){
+            instance = new BoardGame();
+            }
+            return instance;
+    }
+    public static void loadGame(int languageProgress){
+        return 0;
+    }
+    public static void startGame(){
+        return 0;
+    }
+    public static void endGame(){
+        return null;
+    }
+    //steps the player forward on the board
+    //updates the player position 
+    //starts new lesson 
+    public void nextSpace(int steps){
+        player.move(steps, 15 * 15);
+        boardgame.updateBoard(player);
+        startLesson();
+    }
+    public static void saveBoard(){
+        return 0;
+    }
+    }
+    
