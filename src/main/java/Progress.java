baf05a52e5349c9548932c4b8ec51e3d28812da4
Tony <tl27@email.sc.edu>
2024-10-25 15:08:43 -0400

    updated #38

--- src/main/java/Progress.java
+++ src/main/java/Progress.java
@@ -4,11 +4,13 @@ import java.util.HashMap;
 public class Progress{
   private HashMap<Lesson, Integer> lessonProgress;
   private ArrayList<Lesson> lessons;
+  private int languageProgress;
 /** 
  * Constructor initalizes the lesson progress tracker;
  */
   public Progress(){
     this.lessonProgress = new HashMap<>();
+    languageProgress = 0;
    }
 
   public void setLessons(ArrayList<Lesson> lessons) {
@@ -18,14 +20,17 @@ public class Progress{
     }
   }
   public ArrayList<Question> getIncorrect(Question question){
-    return incorrectQuestions;
+    ArrayList<Question> questions =  new ArrayList<Question>();
+    for(Lesson lesson : lessons)
+    
+    return questions;
     }
 
 public void updateProgress( LessonTopic topic, Question question){
   for(Lesson lesson : lessons) {
     if(lesson.getTopic().equals(topic)) {
       if(question instanceof PictureStory) {
-        lessonProgress.put(lesson, null)
+        lessonProgress.put(lesson, null);
       }
         
     }
@@ -34,4 +39,8 @@ public void updateProgress( LessonTopic topic, Question question){
   public Integer getLessonProgress(Lesson lesson){
     return lessonProgress.getOrDefault(lesson, null);
     }
+
+  public int getLanguageProgress() {
+    return this.languageProgress;
+}
 }
