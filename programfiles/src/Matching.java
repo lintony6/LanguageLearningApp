b05585a2acc81575efd582abcf875f9442c7c951
aqw tiqz niqz <bl37@email.sc.edu>
2024-10-27 12:55:35 -0400

    Add pictureStory and Matching with updates

--- programfiles/src/Matching.java
+++ programfiles/src/Matching.java
@@ -2,21 +2,38 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Matching {
-private ArrayList<Word> wordPairs;
+    // Field to store word pairs
+    private ArrayList<Word> wordPairs;
 
-public Matching(ArrayList<Word>){
-    return 0;
-}
-public addWordPair(Word word1, Word word2){
-    return wordPairs;
-}
-public static void removeWordPair(Word word1, Word word2){
-    return 0;
-}
-public HashMap<Word,Word>getContent(){
-    return 0;
-}
-public boolean isCorrect(Word word1, Word word2){
-    return true; 
-}
+    // Constructor
+    public Matching(ArrayList<Word> wordPairs) {
+        this.wordPairs = wordPairs;
+    }
+
+    // Method to add a word pair
+    public void addWordPair(Word word1, Word word2) {
+        wordPairs.add(word1);
+        wordPairs.add(word2);
+    }
+
+    // Method to remove a word pair
+    public void removeWordPair(Word word1, Word word2) {
+        wordPairs.remove(word1);
+        wordPairs.remove(word2);
+    }
+
+    // Method to get the content of word pairs as a HashMap
+    public HashMap<Word, Word> getContent() {
+        HashMap<Word, Word> wordMap = new HashMap<>();
+        for (int i = 0; i < wordPairs.size(); i += 2) {
+            wordMap.put(wordPairs.get(i), wordPairs.get(i + 1));
+        }
+        return wordMap;
+    }
+
+    // Method to check if a word pair matches
+    public boolean isCorrect(Word word1, Word word2) {
+        HashMap<Word, Word> wordMap = getContent();
+        return wordMap.getOrDefault(word1, null) == word2;
+    }
 }
